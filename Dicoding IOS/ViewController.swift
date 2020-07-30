69aa044765386a7f0cd687415fc4f9f2a8dd26ed
jason anggarah <jasonanggarah@gmail.com>
2020-07-30 11:27:23 +0700

    Initial Commit

---
+++ Dicoding IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicoding IOS
+//
+//  Created by PROSIA on 30/07/20.
+//  Copyright © 2020 Jason Dicoding IOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
